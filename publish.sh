3503b386d55c98a4d56379f67b24ad35b6afb664
Chuck Chau <cchau2@gmail.com>
2015-09-08 04:18:34 +0000

    clear repo

--- publish.sh
+++
@@ -1,9 +0,0 @@
-rsync -a --delete \
---delete-excluded .git \
---delete-excluded .gitignore \
---delete-excluded publish.sh \
---delete-excluded CNAME \
-~/sites/savewalnut/src/dist/ .
-git add --all
-git commit -m "published on `date`"
-git push origin master
